49b9a1de5c35ddc498cf2c3abcf72424921e21ac
yulirealman <yulirealman@gmail.com>
2025-11-26 09:02:34 +0800

    TokenInterceptor complete

--- src/main/java/com/realman/SchoolSystem/config/WebConfig.java
+++ src/main/java/com/realman/SchoolSystem/config/WebConfig.java
@@ -15,6 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(tokenInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(tokenInterceptor).addPathPatterns("/**").excludePathPatterns("/login");
     }
 }
