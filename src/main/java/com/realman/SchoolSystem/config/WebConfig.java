6f171e41ba8f153c36818a5754d97d8353832eed
yulirealman <yulirealman@gmail.com>
2025-11-26 09:00:50 +0800

    TokenInterceptor complete

---
+++ src/main/java/com/realman/SchoolSystem/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.realman.SchoolSystem.config;
+
+import com.realman.SchoolSystem.interceptor.TokenInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//this class and TokenInterceptor.java is used for log in functionality
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private TokenInterceptor tokenInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenInterceptor).addPathPatterns("/**");
+    }
+}
---
+++ src/main/java/com/realman/SchoolSystem/interceptor/TokenInterceptor.java
@@ -0,0 +1,43 @@
+package com.realman.SchoolSystem.interceptor;
+
+import com.realman.SchoolSystem.utils.JwtUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class TokenInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI(); // ie. /api/dept/1
+
+        //we don't block /login, because it needs login to have token
+        if(requestURI.contains("/login")){
+            return true;
+        }
+
+        String token = request.getHeader("token");
+
+        //if token is null or empty, return 401
+        if(token == null || token.isEmpty()){
+            response.setStatus(401);
+            return false;
+        }
+
+        //if token exsits we need to check if it's valid
+        try{
+            JwtUtils.parseToken(token);
+        } catch (Exception e){
+            response.setStatus(401);
+            return false;
+        }
+
+
+        return true;
+
+
+    }
+}
